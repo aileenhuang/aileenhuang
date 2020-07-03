5548789da5325e1ca951638339a8483818762983
Aileen <ahuang232@bloomberg.net>
2020-07-02 23:36:38 -0400

    Updates

---
+++ precache-manifest.b93f8b51661b29647e3c6f4dc5bedaba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e64acfaa21e20725bd7f98d7d705e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a48df120edec4b258b8f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80e915fa5fc93350275d",
+    "url": "/static/js/2.bc6ac4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc6ac4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48df120edec4b258b8f",
+    "url": "/static/js/main.74c71031.chunk.js"
+  },
+  {
+    "revision": "d587352d103c4c8d88ba",
+    "url": "/static/js/runtime-main.4aeca1ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
